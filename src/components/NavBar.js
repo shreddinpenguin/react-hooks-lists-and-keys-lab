05733175427bdee1bdd3f413aeb7ff76776bf24f
Taylor Riley <t_riley13@icloud.com>
2023-10-04 14:57:07 -0600

    complete lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,10 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const addLinks = links.map((link) => (
+    <a key={link} href={"#" + link}>{link}</a>
+  ));
+  return <nav>{addLinks}</nav>;
 }
 
 export default NavBar;
